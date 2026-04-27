f99e1f2e912abbd85fc665fa717ba8fe9142c7fb
Abdalah-eslam <abdallaheslam2001@gmail.com>
2026-04-27 17:26:07 +0300

    finish deploy

--- src/config/DBconfig.ts
+++ src/config/DBconfig.ts
@@ -3,3 +3,4 @@ import mongoose  from "mongoose";
 export const DBConnection = mongoose.connect(process.env.MONOGO_URL as string )
 .then(() => console.log('DB connected'))
 .catch((err) => console.log(err))
+
